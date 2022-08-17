3b7a164d96a1f128098510b4bfa0b50776b01043
Prabal Pathak <prabal01pathak@gmail.com>
2022-08-17 12:00:19 +0530

    Time: 178 ms (48.29%), Space: 17.9 MB (52.43%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -4,19 +4,34 @@ class ListNode:
         self.val = val
         self.next = next
         
+# Not optimized
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        l = []
-        for i in lists:
-            cur = i
-            while cur:
-                l.append(cur.val)
-                cur = cur.next
-        l.sort()
+        if len(lists) == 0:
+            return None
+        while len(lists) > 1:
+            merged_lists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if len(lists) > i+1 else None
+                merged_lists.append(self.mergeLists(l1, l2))
+            lists = merged_lists
+        return lists[0]
+    
+    
+    def mergeLists(self, l1, l2):
         dummy = ListNode(0)
-        cur = dummy
-        for i in l:
-            node = ListNode(i)
-            cur.next = node
-            cur = cur.next
+        tail = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
         return dummy.next
