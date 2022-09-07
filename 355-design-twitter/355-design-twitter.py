78cac0e4cb5bf33d1bf393ab297197f818c4e858
Prabal Pathak <prabal01pathak@gmail.com>
2022-09-07 10:39:37 +0530

    Time: 48 ms (51.02%), Space: 14.1 MB (20.48%) - LeetHub

--- 355-design-twitter/355-design-twitter.py
+++ 355-design-twitter/355-design-twitter.py
@@ -3,61 +3,38 @@ import heapq
 
 class Twitter:
     def __init__(self):
-        self.user = defaultdict(set)
-        self.posts = []
-        heapq.heapify(self.posts)
+        self.count = 0
+        self.tweetMap = defaultdict(list)
+        self.followMap = defaultdict(set)
         
-    def check_register(self, userId):
-        if userId not in self.user:
-            self.user[userId] = set()
-            self.user[userId].add(userId)
-
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.check_register(userId)
-        if len(self.posts) == 0:
-            data = [-1, userId, tweetId]
-        else:
-            prev_pri = self.posts[0][0]
-            data = [prev_pri-1, userId, tweetId]
-        heapq.heappush(self.posts, data)
+        self.tweetMap[userId].append([self.count, tweetId])
+        self.count -= 1
         
-    def check_follow(self, userId, followeeId):
-        user = self.user.get(userId, set())
-        if followeeId in user:
-            return True
-        return False
-
     def getNewsFeed(self, userId: int) -> List[int]:
-        self.check_register(userId)
-        res = deque()
-        push_again = []
-        k = 0
-        print(self.posts)
-        while k < 10 and len(self.posts) > 0:
-            data = heapq.heappop(self.posts)
-            if self.check_follow(userId, data[1]):
-                res.append(data[2])
-                k += 1
-            push_again.append(data)
-        for i in push_again:
-            heapq.heappush(self.posts, i)
+        res = []
+        minHeap = []
+        self.followMap[userId].add(userId)
+        for followeeId in self.followMap[userId]:
+            if followeeId in self.tweetMap:
+                index = len(self.tweetMap[followeeId]) -1
+                count, tweetId = self.tweetMap[followeeId][index]
+                minHeap.append([count, tweetId, followeeId, index-1])
+        heapq.heapify(minHeap)
+        while minHeap and len(res) < 10:
+            count, tweetId, followeeId, index = heapq.heappop(minHeap)
+            res.append(tweetId)
+            if index >= 0:
+                count, tweetId = self.tweetMap[followeeId][index]
+                heapq.heappush(minHeap, [count , tweetId, followeeId, index -1])
         return res
 
     def follow(self, followerId: int, followeeId: int) -> None:
-        self.check_register(followerId)
-        self.user[followerId].add(followeeId)
-        print("FolloweeId: ", followeeId)
-        print("Follow: ", self.user)
+        self.followMap[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        self.check_register(followerId)
-        try:
-            self.user[followerId].remove(followeeId)
-        except Exception as _e:
-            pass
-        print("FolloweeId: ", followeeId)
-        print("unfollow: ", self.user)
-        
+        if followeeId in self.followMap[followerId]:
+            self.followMap[followerId].remove(followeeId)
 
 
 # Your Twitter object will be instantiated and called as such:
