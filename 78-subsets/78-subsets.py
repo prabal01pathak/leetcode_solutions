b6976c9240a182c602e9ee1445da0f4065f90dba
Prabal Pathak <prabal01pathak@gmail.com>
2022-09-12 07:10:11 +0530

    Time: 95 ms (5.10%), Space: 14.1 MB (36.05%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -2,8 +2,15 @@ from functools import partial
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first = 0, curr = []):
+            if len(curr) == k:
+                output.append(curr[:])
+            for i in range(first, n):
+                curr.append(nums[i])
+                backtrack(i+1, curr)
+                curr.pop()
+                
+        output = []
         n = len(nums)
-        output = [[]]
-        for cur in nums:
-            output += [[cur]+i for i in output]
+        for k in range(n+1):    backtrack()
         return output
