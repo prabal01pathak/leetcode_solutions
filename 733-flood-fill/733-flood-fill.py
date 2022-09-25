391dfa2ad595cb602c8aa8abca4017ad4086cd06
Prabal Pathak <prabal01pathak@gmail.com>
2022-09-25 10:16:50 +0530

    Time: 80 ms (92.35%), Space: 14.1 MB (38.52%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -2,33 +2,15 @@ from collections import deque
 
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        first = image[sr][sc]
-        visited = set()
-        queue = deque([ (sr, sc) ])
-        count = 0
-        while queue:
-            print(queue)
-            x, y = queue.popleft()
-            if (x, y) in queue:
-                continue
-            visited.add((x, y))
-            value = image[x][y]
-            if first == value:
-                image[x][y] = color
-                if x != len(image) -1:
-                    t = (x+1, y)
-                    if not t in visited:
-                        queue.append(t)
-                if x != 0:
-                    t = (x-1, y)
-                    if not t in visited:
-                        queue.append(t)
-                if y != 0:
-                    t = (x, y-1)
-                    if not t in visited:
-                        queue.append(t)
-                if y != len(image[0])-1:
-                    t = (x, y+1)
-                    if not t in visited:
-                        queue.append(t)
+        row, col = len(image), len(image[0])
+        target_color = image[sr][sc]
+        if target_color == color: return image
+        def dfs(r, c):
+            if image[r][c] == target_color:
+                image[r][c] = color
+                if r >= 1: dfs(r -1, c)
+                if r +1 < row: dfs(r+1, c)
+                if c +1 < col: dfs(r, c+1)
+                if c >= 1: dfs(r, c-1)
+        dfs(sr, sc)
         return image
