551a3b85a29cd8bc5f27e3a163da291dd24de593
Prabal Pathak <prabal01pathak@gmail.com>
2022-09-26 10:30:38 +0530

    Time: 376 ms (12.81%), Space: 16.6 MB (49.26%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -4,24 +4,13 @@ from itertools import product
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        visited = set()
-        def bfs(r, c):
-            if (r, c) in visited: return 0
-            queue = deque([ (r, c) ])
-            area = 0
-            while queue:
-                x, y = queue.popleft()
-                if (x, y) in visited: continue
-                visited.add((x, y))
-                if grid[x][y] == 1:
-                    area += 1
-                    if x > 0 : queue.append((x-1, y))
-                    if x < rows -1: queue.append((x+1, y))
-                    if y < cols-1: queue.append((x, y+1))
-                    if y > 0: queue.append((x, y-1))
-            return area
         max_area = 0
+        def dfs(x, y):
+            if 0 <= x < rows and 0 <= y < cols and grid[x][y]:
+                grid[x][y] = 0
+                return 1 + dfs(x-1, y) + dfs(x+1, y) + dfs(x, y-1) + dfs(x, y+1)
+            return 0
         for row, col in product(range(rows), range(cols)):
-            if grid[row][col] == 1 and (row, col) not in visited:
-                max_area = max(bfs(row, col), max_area)
+            if grid[row][col] == 1:
+                max_area = max(dfs(row, col), max_area)
         return max_area
