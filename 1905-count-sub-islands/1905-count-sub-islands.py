bd3760abfbe3575e206c60e42c1e52879fc1cd99
Prabal Pathak <prabal01pathak@gmail.com>
2022-09-27 11:48:52 +0530

    Time: 8030 ms (10.28%), Space: 109.6 MB (16.48%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -5,26 +5,25 @@ class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         rows, cols = len(grid1), len(grid1[0])
         visited = set()
-        def bfs(r, c):
-            queue = deque([ (r, c) ])
-            is_valid = True
-            while queue:
-                x, y = queue.popleft()
-                if (x, y) in visited:
-                    continue
-                visited.add((x, y))
-                if grid2[x][y] == 1:
-                    if grid1[x][y] != 1:
-                        is_valid = False
-                    if x > 0: queue.append((x-1, y))
-                    if y > 0: queue.append((x, y-1))
-                    if x < rows -1: queue.append((x+1, y))
-                    if y < cols -1: queue.append((x, y+1))
-            return 1 if is_valid else 0
-        res = 0
+        def dfs(r, c):
+            if (
+                r < 0 or r >= rows or c < 0 or c >= cols
+                or grid2[r][c] == 0 or (r, c) in visited
+               ):
+                return True
+            visited.add((r, c))
+            res = True
+            if grid1[r][c] == 0:
+                res = False
+            res = dfs(r-1, c) and res
+            res = dfs(r + 1, c) and res
+            res = dfs(r, c-1) and res 
+            res = dfs(r, c+1) and res
+            return res
+        count = 0
         for row, col in product(range(rows), range(cols)):
-            if grid2[row][col] == 1 and (row, col) not in visited:
-                res += bfs(row, col)
-        return res
+            if grid2[row][col] == 1 and (row, col) not in visited and dfs(row, col):
+                count += 1
+        return count
                 
                 
